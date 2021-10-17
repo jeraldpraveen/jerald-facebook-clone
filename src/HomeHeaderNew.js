663fd10248ce35428ee340d9243075c25b798d8f
jeraldpraveen <jerald.vit@gmail.com>
2021-10-17 15:21:08 +0530

    2.NavBar With Search Icon, Back Icon And Search Field

--- src/HomeHeaderNew.js
+++ src/HomeHeaderNew.js
@@ -1,13 +1,34 @@
 import React from "react";
 import "./HomeHeaderNew.css";
 import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const HomeHeaderNew = (props) => {
   const user = props.user;
   const selected = props.selected;
 
-  const collapseSearch = () => {};
-  const expandSearch = () => {};
+  const collapseSearch = () => {
+    document.getElementsByClassName("homeHeader__logo")[0].style.display =
+      "block";
+    document.getElementsByClassName("homeHeader__searchBack")[0].style.display =
+      "none";
+    document.getElementsByClassName("searchBox")[0].style.display = "none";
+    document.getElementsByClassName("homeHeader__search")[0].style.display =
+      "block";
+    document.getElementsByClassName("dropdown-content3")[0].style.display =
+      "none";
+    document.getElementsByClassName("searchBox")[0].value = "";
+  };
+  const expandSearch = () => {
+    document.getElementsByClassName("homeHeader__logo")[0].style.display =
+      "none";
+    document.getElementsByClassName("homeHeader__searchBack")[0].style.display =
+      "block";
+    document.getElementsByClassName("homeHeader__search")[0].style.display =
+      "none";
+    document.getElementsByClassName("searchBox")[0].style.display = "block";
+  };
   const updateSearchResults = () => {};
   return (
     <div className="homeHeader">
@@ -19,10 +40,10 @@ const HomeHeaderNew = (props) => {
           />
         </Link>
         <div className="homeHeader__searchBack" onClick={collapseSearch}>
-          <i className="searchBackIcon" />
+          <ArrowBackIcon className="searchBackIcon" />
         </div>
         <div className="homeHeader__search" onClick={expandSearch}>
-          <i className="searchIcon" />
+          <SearchIcon className="searchIcon" />
         </div>
         <input
           type="text"
